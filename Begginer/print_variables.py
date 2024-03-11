7ec34e3cf9f24623a1929be6a8cdcfab9627988a
Closdlgdo <cidstp031921@gmail.com>
2024-03-11 04:15:35 -0700

    Python for Hackers Course | Scripting Bug Bounty & Ethical Hacking -- Ryan John

--- Begginer/print_variables.py
+++ Begginer/print_variables.py
@@ -1,2 +1,18 @@
 print("Hello World\nHello Carlos")  # this is a simple string program that also
 # has the \n character that allows us to create a new line.
+
+x = 10
+y = 17
+print(x + y)  # this is a simple addition program.
+
+result = x - y
+print(result)
+
+name = "Carlos"
+print(name)
+
+name = "Carlos"
+last_name = "Delgado"
+print(name + " " + last_name)
+
+print(f"Hello {name} {last_name}")
