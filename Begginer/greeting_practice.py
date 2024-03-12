c18a822872e18325dbec6f40904197ed12ffed2f
Closdlgdo <cidstp031921@gmail.com>
2024-03-12 04:18:18 -0700

    Python for Hackers Course | Scripting Bug Bounty & Ethical Hacking -- Ryan John

---
+++ Begginer/greeting_practice.py
@@ -0,0 +1,9 @@
+# 1. Create a greeting for your program.
+print("Hello, welcome to the Twitter handle generator!")
+# 2. Ask the user for the name of a pet.
+pet = input("What is your pet's name? ")
+# 3. Ask for the name of the city they were born in.
+city = input("What city were you born in? ")
+# 4. Combine the pet name with the word cyber as a new twitter handle and then add the city they are from.
+print(f"Your new twitter handle and bio is @cyber{pet} from {city}.")
+# The output should look like this: "Your new twitter handle and bio is @cyberfred from detroit."
