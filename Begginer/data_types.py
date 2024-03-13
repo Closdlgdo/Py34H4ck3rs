51293230fac89ad547faed05607013a75d3d6796
Closdlgdo <cidstp031921@gmail.com>
2024-03-13 05:51:02 -0700

    Python for Hackers Course | Scripting Bug Bounty & Ethical Hacking -- Ryan John

---
+++ Begginer/data_types.py
@@ -0,0 +1,17 @@
+# Data Types
+# 1. String
+my_string = "Hello, World!"
+# 2. Integer
+my_integer = 42
+# 3. Float
+my_float = 3.14
+# 4. Boolean
+my_boolean = True
+# 5. List
+my_list = [1, 2, 3, 4, 5]
+# 6. Tuple
+my_tuple = (1, 2, 3)
+# 7. Set
+my_set = {1, 2, 3}
+# 8. Dictionary
+my_dict = {'key': 'value'}
