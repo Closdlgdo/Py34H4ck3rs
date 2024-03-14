49e300d7f6072627aaf4beaec6767bd9594b52ac
Closdlgdo <cidstp031921@gmail.com>
2024-03-14 04:26:58 -0700

    Python for Hackers Course | Scripting Bug Bounty & Ethical Hacking -- Ryan John

---
+++ Begginer/input_project.py
@@ -0,0 +1,10 @@
+# We will create a small project that will receive two input ints, then it will output the
+# highest of those two numbers.
+
+print("Enter the first number")
+first_number = int(input())
+
+print("Enter the second number")
+second_number = int(input())
+
+print(f"The highest number is {max(first_number, second_number)}")
