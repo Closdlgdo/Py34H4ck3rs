4c18eacb09caa587dbe42fe7502642511a9f362c
Closdlgdo <cidstp031921@gmail.com>
2024-03-16 04:48:26 -0700

    Python for Hackers Course | Scripting Bug Bounty & Ethical Hacking -- Ryan John

--- Begginer/fruit_lists.py
+++ Begginer/fruit_lists.py
@@ -3,6 +3,11 @@
 # for fruit in fruits:
 #     print(fruit)
 
-# Find all number divisible by 3 from 0 to 100
-for fruit in range(0, 100, 3):
-    print(fruit)
+# # Find all number divisible by 3 from 0 to 100
+# for fruit in range(0, 100, 3):
+#     print(fruit)
+
+# Find all number divisible by 3 from 0 to 100 using an if statement.
+for fruit in range(0, 100):
+    if fruit % 3 == 0:
+        print(fruit)
