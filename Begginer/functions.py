a2518894cfb6ad274e4512c16d9c743a29f2c2ca
Closdlgdo <cidstp031921@gmail.com>
2024-03-17 05:01:16 -0700

    Python for Hackers Course | Scripting Bug Bounty & Ethical Hacking -- Ryan John

---
+++ Begginer/functions.py
@@ -0,0 +1,7 @@
+# What is a function? A function is a block of code which only runs when it is called.
+# The built-in functions for Python are: abs(), all(), any(), ascii(), aiter(), anext(), bin(), bool(), bytearray(),
+# bytes(), callable(), chr(), classmethod(), complex(), compile(), delattr(), dict(), dir(), divmod(), exec(), enumerate(),
+# eval(), filter(), float(), format(), frozenset(), getattr(), globals(),hasattr(), hash(), help(), hex(), id(), input(),
+# int(), isinstance(), issubclass(), iter(), len(), list(), locals(),map(), max(), memoryview(), min(), next(), object(),
+# oct(), open(), ord(), pow(), print(), property(), range(), repr(),reversed(), round(), set(), setattr(), slice(), sorted(),
+# staticmethod(), str(), sum(), super(), tuple(), type(), vars(), zip(), and len().
