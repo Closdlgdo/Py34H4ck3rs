88b5a2c7a722a69ec47b5808002f88a86a3260ba
Closdlgdo <cidstp031921@gmail.com>
2024-03-17 04:52:44 -0700

    Python for Hackers Course | Scripting Bug Bounty & Ethical Hacking -- Ryan John

--- Begginer/fizzybuzzy.py
+++ Begginer/fizzybuzzy.py
@@ -2,3 +2,13 @@
 # If a number is divisible by 3, print "Fizz".
 # If a number is divisible by 5, print "Buzz".
 # If a number is divisible by both 3 and 5, print "FizzBuzz".
+
+for number in range(1, 101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
