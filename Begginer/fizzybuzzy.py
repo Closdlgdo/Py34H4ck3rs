fdf0fc785d0cf6c801a010fca3f49c62cedf0468
Closdlgdo <cidstp031921@gmail.com>
2024-03-19 20:00:28 -0700

    Python for Hackers Course | Scripting Bug Bounty & Ethical Hacking -- Ryan John

--- Begginer/fizzybuzzy.py
+++ Begginer/fizzybuzzy.py
@@ -2,6 +2,8 @@
 # If a number is divisible by 3, print "Fizz".
 # If a number is divisible by 5, print "Buzz".
 # If a number is divisible by both 3 and 5, print "FizzBuzz".\
+# Functions do not need to be in order when the print shows. The important part is
+# when the function is called.
 
 def my_function():
     for i in range(1, 101):
