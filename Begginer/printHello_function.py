63da1af94ae777796480bf836c321dae50c84772
Closdlgdo <cidstp031921@gmail.com>
2024-03-21 03:31:49 -0700

    Python for Hackers Course | Scripting Bug Bounty & Ethical Hacking -- Ryan John

---
+++ Begginer/printHello_function.py
@@ -0,0 +1,15 @@
+# Create a program that can take an input of the users name
+# save the name in a variable
+# pass the variable through a function and print "Hello, [name]"
+import time
+
+
+def my_function(name):
+    print(f"Hello, {name}")
+
+
+print("Program starting...")
+time.sleep(3)
+print("Almost there...")
+time.sleep(5)
+my_function("Carlos")
