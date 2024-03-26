9045e9c6748f9cd8a51d83997d7ef2ff53d39fe7
Closdlgdo <cidstp031921@gmail.com>
2024-03-26 15:05:58 -0700

    Python for Hackers Course | Scripting Bug Bounty & Ethical Hacking -- Ryan John

--- ffuf/fuzzing_peeps.py
+++ ffuf/fuzzing_peeps.py
@@ -1,6 +1,8 @@
 import requests
 import sys
 
-res = requests.get(url=f"http://10.10.11.61/api")
+res = requests.get(url=f"http://10.10.11.61/api/v1")
 
-print(res.text)
+print(res)
+data = res.json()
+print(data)
